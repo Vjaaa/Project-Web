5fcc0e9f1d73563dd4df84429a2f2e96ce5e1d3e
Vjaaa <rafifnabily@gmail.com>
2023-06-14 14:46:22 +0700

    fix seeder database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            DataIspusSeeder::class,
+            // DataIspusSeeder::class,
             UsersSeeder::class,
         ]);
     }
