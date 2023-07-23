9ccd43af36787b9fc4447716161212bdb9daa715
Vjaaa <rafifnabily@gmail.com>
2023-07-23 13:40:28 +0700

    update format upload csv to database

--- app/Imports/DataIspusImport.php
+++ app/Imports/DataIspusImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\DataIspu;
+use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\Importable;
@@ -17,16 +18,16 @@ class DataIspusImport implements ToCollection, WithHeadingRow
     {
         foreach ($rows as $row) {
             DataIspu::create([
-                'tanggal' => $row['tanggal'],
-                'pm10' => $row['pm10'],
-                'so2' => $row['so2'],
-                'co' => $row['co'],
-                'o3' => $row['o3'],
-                'no2' => $row['no2'],
-                'max' => $row['max'],
+                'tanggal' => Carbon::create($row['tanggal']),
+                'pm10' => intval($row['pm10']),
+                'so2' => intval($row['so2']),
+                'co' => intval($row['co']),
+                'o3' => intval($row['o3']),
+                'no2' => intval($row['no2']),
+                'max' => intval($row['max']),
                 'critical' => Str::upper($row['critical']),
                 'categori' => Str::title($row['categori']),
-                'lokasi' => $row['lokasi_spku'],
+                'lokasi' => Str::upper($row['lokasi_spku']),
                 'status_data' => 'Data Latih',
             ]);
         }
