795069234aaca41a3b613e343f00725d624b7206
Vjaaa <rafifnabily@gmail.com>
2023-08-07 17:16:06 +0700

    refactor: change some view

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,37 +3,22 @@
         <div class="d-flex">
             <!-- LOGO -->
             <div class="navbar-brand-box">
-                <a href="/" class="logo logo-light">
+                <a href="{{ route('beranda') }}" class="logo logo-light text-start">
                     <span class="logo-sm">
                         <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('assets/images/logo-light.png') }}" alt="" height="20">
+                        <img src="{{ asset('assets/images/logo-sm.png') }}" alt="" height="32">
                     </span>
                 </a>
             </div>
             <!-- end LOGO -->
         </div>
-        <!-- Search input -->
-        <div class="search-wrap" id="search-wrap">
-            <div class="search-bar">
-                <input class="search-input form-control" placeholder="Search" />
-                <a href="#" class="close-search toggle-search" data-target="#search-wrap">
-                    <i class="mdi mdi-close-circle"></i>
-                </a>
-            </div>
-        </div>
-        <!-- end Search input -->
         <div class="d-flex">
-            <div class="dropdown d-none d-lg-inline-block">
-                <button type="button" class="btn header-item toggle-search noti-icon waves-effect" data-target="#search-wrap">
-                    <i class="mdi mdi-magnify"></i>
-                </button>
-            </div>
             @auth
                 <div class="dropdown d-inline-block">
                     <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img class="rounded-circle header-profile-user" src="{{ asset('assets/images/users/avatar-7.jpg') }}" alt="Header Avatar">
+                        <img class="rounded-circle header-profile-user" src="{{ asset('assets/images/users/avatar-blank.png') }}" alt="Header Avatar">
                         <span class="d-none d-xl-inline-block ms-1">{{ auth()->user()->nama }}</span>
                         <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                     </button>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,17 +1,5 @@
 <div class="vertical-menu">
     <div data-simplebar class="h-100">
-        <div class="user-sidebar text-center">
-            <div class="dropdown">
-                <div class="user-img">
-                    <img src="{{ asset('assets/images/users/avatar-7.jpg') }}" alt="" class="rounded-circle">
-                    <span class="avatar-online bg-success"></span>
-                </div>
-                <div class="user-info">
-                    <h5 class="mt-3 font-size-16 text-white">{{ auth()->user()->nama }}</h5>
-                    <span class="font-size-13 text-white-50">{{ auth()->user()->role }}</span>
-                </div>
-            </div>
-        </div>
         <!--- Sidemenu -->
         <div id="sidebar-menu">
             <!-- Left Menu Start -->
